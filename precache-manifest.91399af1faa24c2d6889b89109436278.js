c86e1eae26724050a3c05dee3cdf145115fb3dcf
fooodStore <80377315+fooodStore@users.noreply.github.com>
2021-03-10 08:24:37 +0330

    Add files via upload

---
+++ precache-manifest.91399af1faa24c2d6889b89109436278.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b134d6b205ea8a0cfa",
+    "url": "/css/app.cf24f068.css"
+  },
+  {
+    "revision": "e415787b033a56d1c06a",
+    "url": "/css/chunk-vendors.8ae48b83.css"
+  },
+  {
+    "revision": "d7a9afe8e4c3cff89d0b68f0a1bb1768",
+    "url": "/img/food_icons/android-icon-192x192-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "6b4be4489e94e6351e1cffd7820e9ff4",
+    "url": "/img/food_icons/apple-icon-114x114-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "ebbd02059d98a1d808e80a1b76a50e13",
+    "url": "/img/food_icons/apple-icon-120x120-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "b8e9780a7b7da67b77b9cf7d6cd2b90c",
+    "url": "/img/food_icons/apple-icon-144x144-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "af4cc9076f28135efe04d2c713247ff4",
+    "url": "/img/food_icons/apple-icon-152x152-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "6a9bd16eb25ba73ae679e7cbaac03a0e",
+    "url": "/img/food_icons/apple-icon-180x180-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "f5f2167a5d909ecaa3db5c50f63cb71f",
+    "url": "/img/food_icons/apple-icon-57x57-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "d10caad5797b18e77a78b42f96d895bb",
+    "url": "/img/food_icons/apple-icon-60x60-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "34924298e401089fa1baa185afb106ff",
+    "url": "/img/food_icons/apple-icon-72x72-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "3f1347a4f5420b830c62acb9ac3855ee",
+    "url": "/img/food_icons/apple-icon-76x76-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "af558eb245922503269ce60afde5b0e2",
+    "url": "/img/food_icons/favicon-16x16-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "4b9c83e2a551afcef1789c5156592693",
+    "url": "/img/food_icons/favicon-32x32-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "db2556ab20381a869c7f70da41edeb24",
+    "url": "/img/food_icons/favicon-96x96-dunplab-manifest-34510.png"
+  },
+  {
+    "revision": "35699303061ba8652b21b9162f2f5f13",
+    "url": "/img/gallery-img-1.35699303.jpeg"
+  },
+  {
+    "revision": "73fcd6ce26c2c57a54e5e77fdb6be29b",
+    "url": "/img/gallery-img-2.73fcd6ce.jpeg"
+  },
+  {
+    "revision": "b427f683e59c432286d404ad5d59ef29",
+    "url": "/img/gallery-img-3.b427f683.jpeg"
+  },
+  {
+    "revision": "abe27d74a81c9c239bb901e052f8449d",
+    "url": "/img/img8.abe27d74.jpeg"
+  },
+  {
+    "revision": "f1cf1da83d8ddcb379a9433c061d2abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b134d6b205ea8a0cfa",
+    "url": "/js/app.fb43cb8c.js"
+  },
+  {
+    "revision": "e415787b033a56d1c06a",
+    "url": "/js/chunk-vendors.91d23330.js"
+  },
+  {
+    "revision": "b019ea9659b6e2a5cac7872bafa185e0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d708767a56facb7a04348539b2bd3b1f",
+    "url": "/style/main.css"
+  }
+]);
